14c12a48196dc38539c0875862f9a208d4083467
Nethra Jagathisan <nethrajagathisan@gmail.com>
2026-02-12 14:00:35 +0530

    feat: add multimodal support

--- txt_gen.py
+++ txt_gen.py
@@ -1,15 +1,17 @@
 from google import genai
 from google.genai import types
+from PIL import Image
 
 client = genai.Client()
 
 prompt = input("Enter your prompt: ")
+image = Image.open("img/cat.jpg")
 
 response = client.models.generate_content(
     model = 'gemini-2.5-flash',
-    contents = prompt,
+    contents = [image, prompt],
     config = types.GenerateContentConfig(
-        system_instruction = "Respond in exactly one line",
+        system_instruction = "Response in a funny way",
         temperature = 0.5
     )
 )
